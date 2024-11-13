19d7ab75c09ede9010f2f29fa9d2ec593770f084
lucashemi <lucasxberger@gmail.com>
2024-11-13 08:48:17 -0300

    bug fixes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import './App.css'
-import Home from './pages/home';
+import Home from './pages/Home';
 import Country from './pages/Country';
 
 const baseUrl = "http://localhost:3001";
